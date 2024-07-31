545e32fd9a594e73d6cae046b7eb1366d8e01ad1
Tevo <tevoguerra@gmail.com>
2024-07-31 20:35:06 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="div">
+    <h1>Janet she be the Queen</h1>
+    <h1>Número de vezes clicadas: {{ count }}</h1>
+    <button @click="onClick">Clica no pae</button>
+    <button @click="clean">Clean</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const onClick = () => {
+  count.value = count.value + 1
+}
+const clean = () => {
+  count.value = 0
+}
+</script>
+
+<style lang="css">
+.div {
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+button {
+  width: 100px;
+}
+</style>
